246774eb5ad5b3bd9d0bec8a4260973ce999ee48
Ethan <45067032+EKO135@users.noreply.github.com>
2020-06-25 11:38:36 +1000

    Update noclose.hpp

--- noclose.hpp
+++ noclose.hpp
@@ -1,14 +1,14 @@
-#define NOCLOSE_HPP
 #ifndef NOCLOSE_HPP
+#define NOCLOSE_HPP
 
 #include <Windows.h>
 
 void grey_x_min()
 {
-    HWND hwnd = GetConsoleWindow();
-    HMENU hmenu = GetSystemMenu(hwnd, FALSE);
-    EnableMenuItem(hmenu, SC_CLOSE, MF_GRAYED);
-    EnableMenuItem(hmenu, SC_MINIMIZE, MF_GRAYED);
+    HWND hwnd = GetConsoleWindow(); // get handle on window
+    HMENU hmenu = GetSystemMenu(hwnd, FALSE); // get handle on that windows action menu
+    EnableMenuItem(hmenu, SC_CLOSE, MF_GRAYED); // grey its X button
+    EnableMenuItem(hmenu, SC_MINIMIZE, MF_GRAYED); // grey its - button
 }
 
 #endif // NOCLOSE_HPP
